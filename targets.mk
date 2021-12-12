2c74af44218e168c358b4ea12d8ed35da292e484
Bryan Bugyi <bryanbugyi34@gmail.com>
2021-12-11 20:13:20 -0500

    Lower required test coverage

--- targets.mk
+++ targets.mk
@@ -27,7 +27,7 @@ define runtests
 		-vv \
 		--cov=src/bugyi/tools \
 		--cov-config=setup.cfg \
-		--cov-fail-under=80 \
+		--cov-fail-under=65 \
 		--cov-report=xml:coverage.xml \
 		--cov-report=term-missing \
 		--cov-branch \
